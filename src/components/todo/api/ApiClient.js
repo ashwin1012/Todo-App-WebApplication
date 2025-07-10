ca123063360dd9e598c84181e9fae282a97bb674
Ashwinkv016 <ashwinkv016@aanksolutions.in>
2025-07-10 22:54:43 +0530

    FourthCommit

--- src/components/todo/api/ApiClient.js
+++ src/components/todo/api/ApiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5050';
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5050';
 
 export const apiClient = axios.create({
   baseURL
